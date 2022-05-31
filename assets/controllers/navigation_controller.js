0570fe6b4f2ee6659eedd5d2083a8d043f272d63
Tomawaq <tom@twq.fr>
2022-05-31 18:11:05 +0200

    close menu when click elsewhere

--- assets/controllers/navigation_controller.js
+++ assets/controllers/navigation_controller.js
@@ -1,8 +1,16 @@
-import { Controller } from '@hotwired/stimulus';
-import '../styles/navigation.scss';
+import { Controller } from '@hotwired/stimulus'
+import '../styles/navigation.scss'
 
 export default class extends Controller {
     connect() {
-        console.log('navigation #connect');
+        // Close menu when click elsewhere
+        const navigationSelector = '[data-controller="navigation"]'
+        const navigation = document.querySelector(navigationSelector)
+        const toggleMenuBtn = navigation.querySelector('input#menu')
+        document.addEventListener('click', e => {
+            if (toggleMenuBtn.checked == false) return
+            if (e.target.matches(`${navigationSelector}, ${navigationSelector} *`) == false)
+                toggleMenuBtn.checked = false
+        })
     }
 }
